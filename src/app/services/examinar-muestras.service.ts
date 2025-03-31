9d3610dac5f4bdea2734ced625d10efdb9a21b6c
DuvanMotavita <duvanfel1@yahoo.com.co>
2025-03-31 00:11:36 -0500

    Added correct method

--- src/app/services/examinar-muestras.service.ts
+++ src/app/services/examinar-muestras.service.ts
@@ -9,14 +9,14 @@ export class ExaminarMuestrasService {
   private http: HttpClient = inject(HttpClient);
   constructor() { }
     public getDataBaseRegister(token:string,task:string): Observable<any> {
-      return this.http.get<any>(`/bonita/API/bpm/userTask/${task}/context`, {
+      return this.http.get<any>(`https://7ddb-190-147-114-10.ngrok-free.app /bonita/API/bpm/userTask/${task}/context`, {
         headers: {
           'X-Bonita-API-Token': token,
         },
       });
     }
     public getDataStorageData(token:string,storageId:string): Observable<any> {
-      return this.http.get<any>(`/bonita/portal/resource/taskInstance/Laboratorio/1.0/Registrar%20Examen/API/bdm/businessData/com.company.model.ExaminarMuestras/${storageId}`, {
+      return this.http.get<any>(`https://7ddb-190-147-114-10.ngrok-free.app/bonita/portal/resource/taskInstance/Laboratorio/1.0/Registrar%20Examen/API/bdm/businessData/com.company.model.ExaminarMuestras/${storageId}`, {
         headers: {
           'X-Bonita-API-Token': token,
         },
